b8ea4639d57214e0ab425e21cd42c48b6842f064
frolo <slaider84@list.ru>
2024-09-29 18:22:15 +0300

    add link style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,38 @@
 import '../../styles/Header.css'
-function Header() {
+import {useState} from "react";
 
+function Header () {
+
+    const [selectedLink, setSelectedLink] = useState('Home');
+
+    const handleLinkClick = (link) => {
+
+        setSelectedLink(link);
+    };
     return (
         <nav className="navbar">
-            <a href="/">Home</a>
-            <a href="#">About</a>
-            <a href="#">Projects</a>
+            <a
+                href="/"
+                onClick={() => handleLinkClick('Home')}
+                className={selectedLink === 'Home' ? 'clicked' : ''}
+            >
+                Home
+            </a>
+            <a
+                href="#"
+                onClick={() => handleLinkClick('About')}
+                className={selectedLink === 'About' ? 'clicked' : ''}
+            >
+
+                About
+            </a>
+            <a
+                href="#"
+                onClick={() => handleLinkClick('Projects')}
+                className={selectedLink === 'Projects' ? 'clicked' : ''}
+            >
+                Projects
+            </a>
         </nav>
     )
 }
